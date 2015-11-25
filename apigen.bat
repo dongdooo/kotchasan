a3d15d400a1a79700b4d392f7f38ed6b4b52072f
Goragod <admin@goragod.com>
2015-11-25 10:22:26 +0700

    Added

--- apigen.bat
+++
@@ -1,5 +0,0 @@
-/mnt/Server/linux/apigen.phar generate -s /mnt/Server/htdocs/kotchasan -d /mnt/Server/htdocs/kotchasan/docs --exclude projects --exclude datas --exclude admin --exclude models --exclude skin --exclude tests --exclude widgets --exclude modules --exclude js --exclude ckeditor
-
-
-
-
